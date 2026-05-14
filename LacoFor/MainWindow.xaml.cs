0f1b851cfe3668402863ac89f10c81480874b19b
Rebeca Barella <rebecabarella13@gmail.com>
2026-05-14 13:47:50 -0300

    nicializa projeto

---
+++ LacoFor/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LacoFor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
+    {
+        var quantidadeTexto = tbQuantidade.Text;
+        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        
+        if (quantidadeSorteios > 1)
+        {
+            quantidadeSorteios = 1;
+        }
+        var sorteador = new Random();
+        for (int contador = 0; contador < quantidadeSorteios; contador++)
+        {
+            tbResultado.Text = sorteador.Next(0,40000000).ToString();
+            await Task.Delay(1000);
+        }
+        
+    }
+}

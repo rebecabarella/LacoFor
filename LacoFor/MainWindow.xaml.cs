37496395f292124e705c13a91c74a71d8da2f48a
Rebeca Barella <rebecabarella13@gmail.com>
2026-05-18 15:54:29 -0300

    adiciona emoticons

--- LacoFor/MainWindow.xaml.cs
+++ LacoFor/MainWindow.xaml.cs
@@ -20,50 +20,63 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-    public decimal saldoInicial = 1600.00M;
-    
 
-    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
+    public decimal saldoInicial = 1600.00M;
+    private string[] emoticons = { "🐯", "🍊", "💎", "💰", "🍒", "🔔" };
+
+private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
-        
-        var quantidadeTexto = tbQuantidade.Text;
-        int quantidadeSorteios;
-        try
+        if (!int.TryParse(tbQuantidade.Text, out var quantidadedeSorteio))
         {
-            quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
-        }
-        catch (FormatException)
-        {
-            MessageBox.Show("Entrada invalida. Coloque apenas números de um para cima!");
+            MessageBox.Show("Coloque apenas valores númericos!");
             return;
         }
-        catch (OverflowException)
-        {
-            MessageBox.Show("Erro! O número digitado é maior que o número suportado!");
-            return;
-        }
+
+
         btnSorteio.IsEnabled = false;
         
-        if (quantidadeSorteios < 1)
-        {
-            quantidadeSorteios = 1;
-        }
-        var sorteador = new Random();
-        for (int contador = 0; contador < quantidadeSorteios; contador++)
-        {
-            if (saldoInicial >= 10)
-            {
-                tbResultado.Text = sorteador.Next(0, 40000000).ToString();
-                saldoInicial -= 10M;
-                tbSaldo.Text = "R$"+saldoInicial.ToString();
-                await Task.Delay(1000);
-            }
-            else
-            {
-                MessageBox.Show("Você não tem dinheiro suficiente para realizar o sorteio!");
-                break;
-            }
-        }
+            if (quantidadedeSorteio < 1) quantidadedeSorteio = 1;
+            var sorteador = new Random();
+            for (var contador = 0; contador < quantidadedeSorteio; contador++)
+                if (saldoInicial >= 10)
+                {
+                   
+                    saldoInicial -= 10M;
+                    tbSaldo.Text = "R$ {saldoInicial}";
+                    
+                    var numeroSorteado =sorteador.Next(40000001);
+
+                    if (numeroSorteado == 28)
+                    {
+                        tbSlot1.Text = emoticons[0];
+                        tbSlot2.Text = emoticons[0];
+                        tbSlot3.Text = emoticons[0];
+                        saldoInicial += 20M;
+                    }
+                    else
+                    {
+                        int slot1, slot2, slot3;
+                        do
+                        {
+                            slot1 = sorteador.Next(emoticons.Length);
+                            slot2 = sorteador.Next(emoticons.Length);
+                            slot3 = sorteador.Next(emoticons.Length);
+                            
+                        } while (slot1 == slot2 && slot2 == slot3);
+                        
+                        tbSlot1.Text = emoticons[sorteador.Next(emoticons.Length)];
+                        tbSlot2.Text = emoticons[sorteador.Next(emoticons.Length)];
+                        tbSlot3.Text = emoticons[sorteador.Next(emoticons.Length)];
+                    }
+                    
+                    await Task.Delay(1000);
+                }
+                else
+                {
+                    MessageBox.Show("Você não tem dinheiro suficiente para realizar o sorteio!");
+                    break;
+                }
+        
 
         btnSorteio.IsEnabled = true;
     }

cb80760a7cf85a535d854ee11509d06c0aac272c
Rebeca Barella <rebecabarella13@gmail.com>
2026-05-20 14:11:23 -0300

    calcula o custo total dinamicamente

--- LacoFor/MainWindow.xaml.cs
+++ LacoFor/MainWindow.xaml.cs
@@ -1,30 +1,26 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LacoFor;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    private const decimal custoSorteio = 10M;
+    private readonly string[] emoticons = { "🐯", "🍊", "💎", "💰", "🍒", "🔔" };
+
+    public decimal saldoInicial = 1600.00M;
+
     public MainWindow()
     {
         InitializeComponent();
+        tbSaldo.Text = $"R$ {saldoInicial}";
+        tbcusto.Text = $"R$ {custoSorteio}";
     }
 
-    public decimal saldoInicial = 1600.00M;
-    private string[] emoticons = { "🐯", "🍊", "💎", "💰", "🍒", "🔔" };
-
-private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
+    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
         if (!int.TryParse(tbQuantidade.Text, out var quantidadedeSorteio))
         {
@@ -34,50 +30,56 @@ private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
 
 
         btnSorteio.IsEnabled = false;
-        
-            if (quantidadedeSorteio < 1) quantidadedeSorteio = 1;
-            var sorteador = new Random();
-            for (var contador = 0; contador < quantidadedeSorteio; contador++)
-                if (saldoInicial >= 10)
-                {
-                   
-                    saldoInicial -= 10M;
-                    tbSaldo.Text = "R$ {saldoInicial}";
-                    
-                    var numeroSorteado =sorteador.Next(40000001);
 
-                    if (numeroSorteado == 28)
-                    {
-                        tbSlot1.Text = emoticons[0];
-                        tbSlot2.Text = emoticons[0];
-                        tbSlot3.Text = emoticons[0];
-                        saldoInicial += 20M;
-                    }
-                    else
-                    {
-                        int slot1, slot2, slot3;
-                        do
-                        {
-                            slot1 = sorteador.Next(emoticons.Length);
-                            slot2 = sorteador.Next(emoticons.Length);
-                            slot3 = sorteador.Next(emoticons.Length);
-                            
-                        } while (slot1 == slot2 && slot2 == slot3);
-                        
-                        tbSlot1.Text = emoticons[sorteador.Next(emoticons.Length)];
-                        tbSlot2.Text = emoticons[sorteador.Next(emoticons.Length)];
-                        tbSlot3.Text = emoticons[sorteador.Next(emoticons.Length)];
-                    }
-                    
-                    await Task.Delay(1000);
+        if (quantidadedeSorteio < 1) quantidadedeSorteio = 1;
+        var sorteador = new Random();
+        for (var contador = 0; contador < quantidadedeSorteio; contador++)
+            if (saldoInicial >= custoSorteio)
+            {
+                saldoInicial -= custoSorteio;
+                tbSaldo.Text = "R$ {saldoInicial}";
+
+                var numeroSorteado = sorteador.Next(40000001);
+
+                if (numeroSorteado == 28)
+                {
+                    tbSlot1.Text = emoticons[0];
+                    tbSlot2.Text = emoticons[0];
+                    tbSlot3.Text = emoticons[0];
+                    saldoInicial += custoSorteio * 2;
                 }
                 else
                 {
-                    MessageBox.Show("Você não tem dinheiro suficiente para realizar o sorteio!");
-                    break;
+                    int slot1, slot2, slot3;
+                    do
+                    {
+                        slot1 = sorteador.Next(emoticons.Length);
+                        slot2 = sorteador.Next(emoticons.Length);
+                        slot3 = sorteador.Next(emoticons.Length);
+                    } while (slot1 == slot2 && slot2 == slot3);
+
+                    tbSlot1.Text = emoticons[sorteador.Next(emoticons.Length)];
+                    tbSlot2.Text = emoticons[sorteador.Next(emoticons.Length)];
+                    tbSlot3.Text = emoticons[sorteador.Next(emoticons.Length)];
                 }
-        
+
+                await Task.Delay(1000);
+            }
+            else
+            {
+                MessageBox.Show("Você não tem dinheiro suficiente para realizar o sorteio!");
+                break;
+            }
+
 
         btnSorteio.IsEnabled = true;
+    }
+
+    private void TbQuantidade_OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (!int.TryParse(tbQuantidade.Text, out var quantidadedeSorteio) || quantidadedeSorteio < 1)
+            quantidadedeSorteio = 1;
+        var custoTotal = quantidadedeSorteio * custoSorteio;
+        tbcusto.Text = $"R$ {custoTotal}";
     }
 }
